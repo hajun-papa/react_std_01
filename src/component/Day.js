62856b5e425895ba167f5e18dc7968d8aca8231e
hajun-papa <shhong@telnetware.com>
2025-01-23 12:55:12 +0900

    20250123-1254

--- src/component/Day.js
+++ src/component/Day.js
@@ -10,6 +10,7 @@ export default function Day() {
     //const wordList = dummy.words.filter(word=>(word.day === Number(day)));
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
     
+
     return (<>
     <h2>Day {day}</h2>
     {words.length === 0 && <span>Loading...</span>}
